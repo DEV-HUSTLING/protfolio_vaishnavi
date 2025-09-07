596dbd6fa87db18157995da8eab869815973ac62
DEV-HUSTLING <anushkadevg@gmail.com>
2025-09-07 11:40:21 -0400

    first mvp

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,99 @@
+'use client'
 import Image from "next/image";
+import Testimonial from "./testimonial/page"
+import React, { useEffect, useRef } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const sectionToScrollToRef = useRef(null)
+  useEffect(() => {
+    AOS.init();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    if (sectionToScrollToRef.current) {
+      sectionToScrollToRef.current.scrollIntoView({ behavior: 'smooth' });
+
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col" ref={sectionToScrollToRef}>
+      <header className="fixed bg-gray-300 w-full flex float-right ">
+        <nav className="flex gap-12 p-4 w-full text-lg justify-end">
+          <a className="cursor-pointer" href=""><u>Know Me More</u></a>
+          <a  className="cursor-pointer" href=""><u>Resume</u></a>
+        </nav>
+      </header>
+      <div className="font-sans flex  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 " >
+        <div className="w-full">
+          <Image
+            data-aos="fade-right"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+            src='/titleImgpng.png'
+            alt="Title Img"
+            width={600}
+            height={600}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="Title-Content w-full">
+          <span className="title flex flex-col gap-8 width-fit">
+            <h1
+              data-aos="fade-right"
+              data-aos-offset="200"
+              data-aos-delay="100"
+              data-aos-duration="2000"
+              data-aos-easing="ease-in-out"
+              data-aos-mirror="true"
+              data-aos-once="false"
+              className="text-6xl bg-yellow-300 p-2 w-fit">Hi</h1>
+            <h1
+              data-aos="fade-right"
+              data-aos-offset="200"
+              data-aos-delay="60"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              data-aos-mirror="true"
+              data-aos-once="false"
+              className="text-6xl bg-yellow-300 p-2 w-fit ">I am</h1>
+            <h1
+              data-aos="fade-right"
+              data-aos-offset="100"
+              data-aos-delay="200"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              data-aos-once="false"
+              className="text-6xl bg-yellow-300 p-2 w-fit ">Vaishnavi Reddy</h1>
+          </span>
+          <span className="caption w-full">
+            <p 
+            data-aos="fade-down"
+              data-aos-offset="100"
+              data-aos-delay="1000"
+              data-aos-duration="3000"
+              data-aos-easing="ease-in-out"
+              data-aos-once="false"
+            className="text-lg italic">Full-Time Mom and LinkedIn Brand Strategist — Helping C-Suite Leaders & Working Moms Build Trusted Personal Brands Through Storytelling and Strategic Growth.</p>
+          </span>
+        </div>
+      </div>
+      {/* new section */}
+      <Testimonial />
+      <footer className="text-grey italic text-sm p-4 text-center flex flex-col justify-center items-center">
+        <div className="flex items-center gap-8">
+          {/* <a href="https://www.linkedin.com/in/anushka-gangidi-a7377277" target="blank"><Image
+          src="/LinkedIn_icon.png"
+          width={30}
+          height={30}
+          alt="logo"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          </a> */}
+        </div>
+        <p>Developed by Anushka</p>
       </footer>
     </div>
   );
---
+++ src/app/testimonial/page.js
@@ -0,0 +1,74 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import '../globals.css'
+import Image from "next/image";
+import { motion,AnimatePresence } from "motion/react";
+
+function Page() {
+  const Img=[
+    {src:'/testimonial/evi1.png',caption:'img-1'},
+    {src:'/testimonial/evi2.png',caption:'img-1'},
+    {src:'/testimonial/evi3.png',caption:'img-1'},
+  ]
+  const[curr, setCurr] = useState(0)
+  const[autoplay, setAutoplay] =useState(true)
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+    setCurr((prev)=>(prev+1)%Img.length)
+    },3000)
+    return ()=>clearInterval(timer)
+  },[autoplay])
+  return (
+    <div className='h-screen overflow-hidden'>
+    <div className="testimonial flex align-center justify-center h-1/2 bg-black p-4">
+        <p className='mt-24 w-full sm:w-full md:w-2/5 text-4xl text-center text-white'>
+            I will listen to your words,
+            Understand you by heart,
+            and build your brand that
+            reflects your true value.
+        </p>
+    </div>
+    <div className='flex items-center w-full justify-center h-1/2'>
+    <div 
+    data-aos="flip-down"
+              data-aos-offset="100"
+              data-aos-delay="100"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              data-aos-once="false"
+    
+    className='scoller relative w-full h-full flex items-center justify-center'>
+      {Img.map((it,index)=> 
+      {
+        const offset = index - curr;
+          let transform = `translateX(${offset * 60}%) scale(${1 - Math.abs(offset) * 0.2})`;
+          let blur = Math.min(Math.abs(offset) * 3, 6);
+          let zIndex = 100 - Math.abs(offset);
+      return(
+      <motion.div 
+      key={index}
+        className="absolute transition-all duration-500 ease-in-out"
+        animate={{ opacity: offset === 0 ? 1 : 0.7 }}
+        style={{
+                zIndex,
+                filter: `blur(${blur}px)`,
+                transform,
+              }}
+      >
+        <Image
+        src={it.src}
+        width={600}
+        height={150}
+        alt='evidence'
+        className='rounded-lg'
+        />
+      </motion.div>
+      )
+    })}
+    </div>
+    </div>
+ </div>
+  )
+}
+
+export default Page
